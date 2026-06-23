e53b4cd1a621df36a935a38031190ab4e8e2a03c
Shadow091101 <manavnaik442@gmail.com>
2026-06-23 12:18:03 +0530

    changed a test-api.sh to see mistakes

--- backend/scripts/test-api.sh
+++ backend/scripts/test-api.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-set -e # //This is the key( makes script stop on error)
+set -euo # //This is the key( makes script stop on error)
 
 BACKEND_URL="http://host.docker.internal:9009"
 
 echo "Testing Root Endpoint..."
-curl -f $BACKEND_URL/ || exit 1
+curl -f $BACKEND_URL/
 
 echo -e "\n\nTesting CPU Endpoint..."
-curl -f $BACKEND_URL/cpu || exit 1
+curl -f $BACKEND_URL/cpu 
 
 echo -e "\n\nTesting CPU Threads Endpoint..."
-curl -f $BACKEND_URL/cpu-thread || exit 1
+curl -f $BACKEND_URL/cpu-thread
 
 echo -e "\n\nTesting Network Interfaces Endpoint..."
-curl -f $BACKEND_URL/network-interfaces || exit 1
+curl -f $BACKEND_URL/network-interfaces
 
 echo -e "\n\nAll tests completed."
